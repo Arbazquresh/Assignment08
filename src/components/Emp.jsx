9a74a79f4937e55a2bbf63d5a79163f701b73082
arbazquresh <arbazsoftware65@gmail.com>
2024-08-16 12:53:16 +0530

    First Commit

--- src/components/Emp.jsx
+++ src/components/Emp.jsx
@@ -1,14 +1,12 @@
 import React from "react"
 import { TextField,Button} from "@mui/material"
 export const Emp=()=>{
-   return(
-      <div className="sub">
-      <TextField id="outlined-basic" label="First Name" variant="outlined" />
-      <TextField id="outlined-basic" label="Last Name" variant="outlined"  />
-      <Button variant="contained">Submit</Button>
-      <Button variant="contained">cancel</Button>
-      
-      
-      </div>
-   )
+   return (
+     <div className="sub">
+       <TextField id="outlined-basic" label="First Name" variant="outlined" />
+       <TextField id="outlined-basic" label="Last Name" variant="outlined" />
+       <Button variant="contained">Submit</Button>
+       <Button variant="contained">Cancel</Button>
+     </div>
+   );
 }
